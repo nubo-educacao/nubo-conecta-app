4c5f31f693ded97468188d5bd859758ca26fe29c
Gabriel <gabriel.ferreira.audio@gmail.com>
2026-06-24 17:35:18 -0300

    chore: remove temp push script

--- push.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "d:\Documentos\GitHub\Nubo Workspace\nubo-conecta-app"
-git add -A
-git commit -m "fix: remove duplicate useAuth import and variable declaration in DadosTab"
-git pull --rebase origin main
-git push origin main
